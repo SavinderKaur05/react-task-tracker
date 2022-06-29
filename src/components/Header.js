bce8400a0e105321f7937080c6bf53519214da94
SavinderKaur05 <savinderk@codebrigade.in>
2022-06-29 12:12:10 +0530

    .Completed task creator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 
 import PropTypes from 'prop-types'
+import Button  from './Button'
+import { useLocation } from 'react-router-dom'
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
+
+  const location  = useLocation()
+
+    const onClick = () => {
+        console.log('Click')
+    }
   return (
     <div>
-       <header>
+       <header className='header'>
         <h1>{title}</h1>
-       </header> 
+        {location.pathname === '/' && (<Button color={showAdd ? 'red':'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/> )}
+       </header>  
     </div>
   )
 }
@@ -19,5 +28,11 @@ Header.propTypes = {
      title: PropTypes.string.isRequired
 
 }
+
+// const headingStyle = 
+// {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
 
 export default Header
